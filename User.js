0c2aae6794d316dcf14326fba8170b87c06340a3
lucasjstn <lucas7justino@gmail.com>
2023-06-27 16:25:46 -0300

    examples of how to create more robust schemas

--- User.js
+++ User.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose")
 
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String
+})
 //create a schema
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  email: String,
+  createdAt: String,
+  updatedAt: String,
+  bestFriend: mongoose.SchemaTypes.ObjectId,
+  hobbies: [String],
+  //using a schema as property
+  address: addressSchema 
 })
 
 //create and export the model of that schema
